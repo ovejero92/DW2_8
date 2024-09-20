9d67d3b296db28835f959785dac5b6d5660949c4
Gustavo <gustavo.ovejero@educacion.local>
2024-09-20 19:50:52 -0300

    nav

---
+++ assets/main.js
@@ -0,0 +1,18 @@
+
+//alert("Hola desde main.js")
+console.error('error');
+
+let X = 10;
+const Y = 5;
+
+let nombre = "Gusti";
+let edad = 31;
+
+X = "Alexis";
+
+console.log("Tu nombre es: " + X + "Y tu edad es: " + edad);
+console.log(`Tu nombre es: ${nombre} y tu edad es: ${edad}`);
+
+const NU = prompt('ingrese su nombre:')
+
+console.log(NU)
